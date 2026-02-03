e4416e33b7ff3127bf19f9ee7cbfb8d3e9f3b2e3
anantgupta30 <63636022+anantgupta30@users.noreply.github.com>
2026-02-03 19:22:06 +0530

    Delete q3 directory

--- q3/convert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(dirname "$0")
-
-python3 "$SCRIPT_DIR/vectorizer.py" "$1" "$2" "$3"
--- q3/generate_candidates.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(dirname "$0")
-
-python3 "$SCRIPT_DIR/search.py" "$1" "$2" "$3"
